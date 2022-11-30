2cf09c46b23c261b996a63b263567b7c5c317775
Rodgers31 <otienor986@gmail.com>
2022-11-29 22:36:22 -0600

    Added tailwind css and setup routing for pages

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text className='text-red-500'>Homescreen</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
