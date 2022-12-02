2d2e302e3691fbbfd324740a0ed7b10bb4a4ebb3
Rodgers31 <otienor986@gmail.com>
2022-12-02 17:48:03 -0600

    Added icons and fixed SafeAreaView

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,38 +1,60 @@
-import { useNavigation } from '@react-navigation/native';
+import { Text, View, Image, TextInput, SafeAreaView } from 'react-native';
 import React, { useLayoutEffect } from 'react';
-import { Text, View, Image } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
 
-function HomeScreen() {
+import {
+  UserIcon,
+  ChevronDownIcon,
+  AdjustmentsVerticalIcon,
+  MagnifyingGlassCircleIcon,
+  MagnifyingGlassPlusIcon,
+} from 'react-native-heroicons/outline';
+
+const HomeScreen = () => {
   const navigation = useNavigation();
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
-  });
+  }, []);
+
   return (
-    <SafeAreaView>
+    <SafeAreaView className='bg-white pt-5'>
       <Text className='text-red-500'>
         {/* Header */}
-
         <View className='flex-row pb-3 items-center mx-4 space-x-2'>
           <Image
             source={{
-              url: 'https://links.papareact.com/wru',
+              uri: 'https://links.papareact.com/wru',
             }}
             className='h-7 w-7 bg-gray-300 p-4 rounded-full'
           />
-          <View>
+
+          <View className='flex-1'>
             <Text className='font-bold text-gray-400 text-xs'>
               Deliver Now!
             </Text>
-            <Text className='font-bold text-xl'>Current Location</Text>
+            <Text className='font-bold text-xl'>
+              Current Location
+              <ChevronDownIcon size={20} color='#00CCBB' />
+            </Text>
           </View>
+
+          <UserIcon size={35} color='#00CCBB' />
+        </View>
+
+        {/* Search */}
+        <View>
+          <View>
+            <MagnifyingGlassPlusIcon />
+            <TextInput />
+          </View>
+          <AdjustmentsVerticalIcon color='#00CCBB' />
         </View>
       </Text>
     </SafeAreaView>
   );
-}
+};
 
 export default HomeScreen;
